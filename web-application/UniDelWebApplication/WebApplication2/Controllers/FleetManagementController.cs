f9a9967c7c8e57a989ed4ee8790bc208f4d72306
u17283818 <u17283818@tuks.co.za>
2020-07-10 10:42:05 +0200

    Courier ID established

--- web-application/UniDelWebApplication/WebApplication2/Controllers/FleetManagementController.cs
+++ web-application/UniDelWebApplication/WebApplication2/Controllers/FleetManagementController.cs
@@ -37,8 +37,8 @@ namespace UniDelWebApplication.Controllers
             Console.WriteLine(cV.Count);
             foreach (var ve in cV)
             {
-                Console.WriteLine(ve.CourierCompany);
-                //if (ve.CourierCompany.CourierCompanyID == 3)
+                Console.WriteLine(ve.CourierCompany.UserID);
+                //if (ve.CourierCompany.UserID == 3)
                   //  Console.WriteLine(ve);
             }
             Console.WriteLine("---------------------------------");
