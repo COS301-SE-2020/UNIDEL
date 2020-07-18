d82b0fb1af8562fbd1440470b592ece932fc6c6c
William Tandwe <u15232672@tuks.co.za>
2020-07-18 11:58:11 +0200

    Mobile application with all files needed to run it

---
+++ mobile-application/UniDel/UniDel/UniDel/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using UniDel.Services;
+using UniDel.Views;
+
+namespace UniDel
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
