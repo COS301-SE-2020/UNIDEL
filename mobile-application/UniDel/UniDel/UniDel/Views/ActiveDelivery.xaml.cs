e4435d9ca7bf8f997d2220f01aeda70d3184603d
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-07-23 04:23:48 +0200

    Delivery Pages done
    No API calls implemented yet

---
+++ mobile-application/UniDel/UniDel/UniDel/Views/ActiveDelivery.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UniDel.Views
+{
+    public partial class ActiveDelivery : ContentPage
+    {
+        public ActiveDelivery()
+        {
+            InitializeComponent();
+        }
+
+        void Complete_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+
+    }
+}
