cc5f735f51818c7499de58f2a3b16fb82a867f28
u17283818 <u17283818@tuks.co.za>
2020-07-23 09:26:45 +0200

    Progress so far
    Working API with link:
    https://memoryinjectllamas.carto.com/builder/9c8296c3-1e37-43b0-8327-5ec2a868dd57/embed
    
    Cannot connect API to mobile app

--- mobile-application/UniDel/UniDel/UniDel/Views/MapPage.xaml.cs
+++ mobile-application/UniDel/UniDel/UniDel/Views/MapPage.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using RestSharp;
 using Xamarin.Forms;
 
 namespace UniDel.Views
 {
     public partial class MapPage : ContentPage
     {
+        private static RestClient client = new RestClient("https://unidelapi.azurewebsites.net/api/");
+
         public MapPage()
         {
             InitializeComponent();
         }
+
+        void viewMap(object sender, EventArgs e)
+        {
+            RestRequest request = new RestRequest("Default", Method.GET);
+            //IRestResponse<List<string>> K = client.Execute<List<string>>(request);
+            IRestResponse<string> K = client.Execute<string>(request);
+            //String K = "Khakhu";
+            (sender as Button).Text = K.ToString();
+        }
+
     }
 }
