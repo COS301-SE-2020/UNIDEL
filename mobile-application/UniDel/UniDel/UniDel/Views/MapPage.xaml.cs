ae569c0f2decf6a1cc5f9aec1fcfb189ecc499ae
u17283818 <u17283818@tuks.co.za>
2020-07-23 11:27:54 +0200

    Working

--- mobile-application/UniDel/UniDel/UniDel/Views/MapPage.xaml.cs
+++ mobile-application/UniDel/UniDel/UniDel/Views/MapPage.xaml.cs
@@ -12,15 +12,6 @@ namespace UniDel.Views
         public MapPage()
         {
             InitializeComponent();
-
-            /*WebView webView = new WebView
-            {
-                /*Source = new UrlWebViewSource
-                {
-                    Url = "https://memoryinjectllamas.carto.com/builder/9c8296c3-1e37-43b0-8327-5ec2a868dd57/embed",
-                }
-                Source= "https://memoryinjectllamas.carto.com/builder/9c8296c3-1e37-43b0-8327-5ec2a868dd57/embed",
-            };*/
         }
 
         void webviewNavigating(object sender, WebNavigatingEventArgs e)
@@ -32,15 +23,5 @@ namespace UniDel.Views
         {
             labelLoading.IsVisible = false;
         }
-
-        void viewMap(object sender, EventArgs e)
-        {
-            RestRequest request = new RestRequest("Default", Method.GET);
-            //IRestResponse<List<string>> K = client.Execute<List<string>>(request);
-            IRestResponse<string> K = client.Execute<string>(request);
-            //String K = "Khakhu";
-            (sender as Button).Text = K.ToString();
-        }
-
     }
 }
