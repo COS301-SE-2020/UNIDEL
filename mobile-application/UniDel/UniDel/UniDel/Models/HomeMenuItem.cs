4d7c602b4d21c1d692d1cc19f70bb7caae203567
u17028397 <55739669+u17028397@users.noreply.github.com>
2020-08-13 12:38:56 +0200

    QRPage to Navigation Bar added

--- mobile-application/UniDel/UniDel/UniDel/Models/HomeMenuItem.cs
+++ mobile-application/UniDel/UniDel/UniDel/Models/HomeMenuItem.cs
@@ -7,6 +7,7 @@ namespace UniDel.Models
     public enum MenuItemType
     {
         Browse,
+        QRCode,
         Deliveries,
         Map,
         About
---
+++ mobile-application/UniDel/UniDel/UniDel/Views/QRScanningPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UniDel.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRScanningPage : ContentPage
+    {
+        public QRScanningPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
