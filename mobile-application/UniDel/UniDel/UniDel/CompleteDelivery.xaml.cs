84fa2453746faaaae775937a6e015d4390c97147
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-08-17 09:18:20 +0200

    Updated

---
+++ mobile-application/UniDel/UniDel/UniDel/CompleteDelivery.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using UniDel.ViewModels;
+using Xamarin.Forms;
+
+namespace UniDel.Views
+{
+    public partial class CompleteDelivery : ContentPage
+    {
+        public ObservableCollection<CompleteDeliveryViewModel> complete_deliveries { get; set; }
+
+        public CompleteDelivery()
+        {
+            InitializeComponent();
+            Navigation.PopToRootAsync();
+
+            BindingContext = this;
+
+            //apidata = [{ }]
+            //foreach (x) {
+            //    complete_deliveries.Add(new CompleteDeliveryViewModel
+            //    { deliveryID = "Delivery ID: "+ X.id,
+            //        pickupName = "Pickup : " + X.pickupname,
+            //        dropoffName = "Drop Off : " + X.dropoffname });
+
+            //}
+
+            complete_deliveries = new ObservableCollection<CompleteDeliveryViewModel>();
+
+            complete_deliveries.Add(new CompleteDeliveryViewModel
+                { deliveryID = "1", pickupName = "BLEH2", dropoffName = "BLEH" });
+            complete_deliveries.Add(new CompleteDeliveryViewModel
+            { deliveryID = "2", pickupName = "BLEH2", dropoffName = "BLEH" });
+            complete_deliveries.Add(new CompleteDeliveryViewModel
+            { deliveryID = "3", pickupName = "BLEH2", dropoffName = "BLEH" });
+            complete_deliveries.Add(new CompleteDeliveryViewModel
+            { deliveryID = "4", pickupName = "BLEH2", dropoffName = "BLEH" });
+            complete_deliveries.Add(new CompleteDeliveryViewModel
+            { deliveryID = "5", pickupName = "BLEH2", dropoffName = "BLEH" });
+
+            completeView.ItemsSource = complete_deliveries;
+        }
+    }
+}
---
+++ mobile-application/UniDel/UniDel/UniDel/DeliveryDetails.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using UniDel.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace UniDel.Views
+{
+    public partial class DeliveryDetails : ContentPage
+    {
+        public ObservableCollection<DeliveryDetailsViewModel> delivery_details { get; set; }
+
+        public DeliveryDetails()
+        {
+            InitializeComponent();
+            Navigation.PopToRootAsync();
+            BindingContext = this;
+
+            //apidata = [{ }]
+            //foreach (x) {
+            //    complete_deliveries.Add(new CompleteDeliveryViewModel
+            //    { deliveryID = "Delivery ID: "+ X.id,
+            //        pickupName = "Pickup : " + X.pickupname,
+            //        dropoffName = "Drop Off : " + X.dropoffname });
+
+            //}
+
+            delivery_details = new ObservableCollection<DeliveryDetailsViewModel>();
+
+            delivery_details.Add(new DeliveryDetailsViewModel
+            { deliveryID = "Delivery ID : 1", pickupName = "The Daily Sun", dropoffName = "Engen", dropofLocation = "178 Belly Avenue, Krooyling, Pretoria, 0083",
+              pickupLocation = "26 Pieter Drive, Republic, Pretoria, 0847"});
+
+
+
+        }
+
+        async void Complete_Clicked(object sender, EventArgs e)
+        {
+            //code here to remove object from list
+            await DisplayAlert("Confirmation", "Delivery Complete", "OK");
+            await Navigation.PopAsync();
+        }
+
+
+    }
+}
