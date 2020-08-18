4dcd822af06729df03f4666453087ddc8077ab6f
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-08-18 21:49:31 +0200

    updated wirh api

---
+++ api 2/UniDelAPI/UniDelAPI/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniDelAPI.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Column("UserID")]
+        [Key]
+        [DatabaseGenerated
+            (DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int UserID { get; set; }
+
+        [Column("UserEmail")]
+        [StringLength(255)]
+        public string UserEmail { get; set; }
+
+        [Column("UserPassword")]
+        [StringLength(255)]
+        public string UserPassword { get; set; }
+
+        [Column("UserProfilePic")]
+        public byte[] UserProfilePic { get; set; }
+
+        [Column("UserType")]
+        [StringLength(255)]
+        public string UserType { get; set; }
+    }
+}
