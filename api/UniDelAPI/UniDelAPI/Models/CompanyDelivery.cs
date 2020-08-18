5a9a4ac54cc368b4baa55df71c07de080f5342ff
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-08-18 20:49:35 +0200

    Added API

---
+++ api/UniDelAPI/UniDelAPI/Models/CompanyDelivery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniDelAPI.Models
+{
+    [Table("CompanyDelivery")]
+    public class CompanyDelivery
+    {
+        [Column("CompanyDeliveryID")]
+        [Key]
+        [DatabaseGenerated
+            (DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int CompanyDeliveryID { get; set; }
+
+        [Column("CourierCompanyID")]
+        public int CourierCompanyID { get; set; }
+
+        [ForeignKey("CourierCompanyID")]
+        public CourierCompany CourierCompany { get; set; }
+
+        [Column("DeliveryID")]
+        public int DeliveryID { get; set; }
+
+        [ForeignKey("DeliveryID")]
+        public Delivery Delivery { get; set; }
+    }
+}
