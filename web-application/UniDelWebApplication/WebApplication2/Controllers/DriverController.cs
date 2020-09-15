1b3f59940926cd1ff5ac0e5be76af957cb0d4754
Khakhu Ramakuela <56280844+u17283818@users.noreply.github.com>
2020-09-15 23:53:10 +0200

    Simple driver table

--- web-application/UniDelWebApplication/WebApplication2/Controllers/DriverController.cs
+++ web-application/UniDelWebApplication/WebApplication2/Controllers/DriverController.cs
@@ -22,11 +22,58 @@ namespace UniDelWebApplication.Controllers
             uniDelDb = db;
         }
 
-        // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Driver> d = filterDrivers();
+            return View(d);
+        }
 
-            return View();
+        //helper function to use user id to find courier company id
+        private int findCompany(int sesID)
+        {
+            List<CourierCompany> cC = uniDelDb.CourierCompanies.ToList();
+            foreach (var cmp in cC)
+            {
+                if (cmp.UserID == sesID)
+                    return cmp.CourierCompanyID;
+            }
+            return -1;
+        }
+
+
+        //Helper function to filter drivers
+        private List<Driver> filterDrivers()
+        {
+            try
+            {
+                Console.WriteLine(uniDelDb.CourierCompanies.Find(int.Parse(HttpContext.Session.GetString("ID"))));//Does not work without this, I don't know why
+                List<CompanyDriver> cD = uniDelDb.CompanyDrivers.ToList();
+                List<CompanyDriver> myDriv = new List<CompanyDriver>();
+                int comID = findCompany(int.Parse(HttpContext.Session.GetString("ID")));
+                foreach (var dr in cD)
+                {
+                    if (dr.CourierCompanyID == comID)
+                        myDriv.Add(dr);
+                }
+                List<Driver> driv = new List<Driver>();
+                foreach (var dr in myDriv)
+                {
+                    driv.Add(uniDelDb.Drivers.Find(dr.DriverID));
+                }
+                return driv;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public IActionResult DriverLocation()
+        {
+            String Location = "-25.756257400000003,28.240955699999997";
+            String final = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBO2eSzL22PogFFqA30bXPrWwvTbqpMYHM&q="+Location;
+            return View((object)final);
         }
     }
 }
