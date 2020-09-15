df97ff4a8df2b83782a703bd2814b4f7d643b32f
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-09-15 12:44:50 +0200

    cache

--- mobile-application/UniDel/UniDel/UniDel/Views/RegisterPage.xaml.cs
+++ mobile-application/UniDel/UniDel/UniDel/Views/RegisterPage.xaml.cs
@@ -236,19 +236,19 @@ namespace UniDel.Views
 
         public void OnRegEmailTextChanged(object sender, EventArgs args)
         {
-            if (Patterns.EmailAddress.Matcher(regEmail.Text).Matches())
-            {
-                regEmail.BackgroundColor = Color.FromHex("#88fc8c");
-                formChecks[0] = true;
-                if (allChecksClear()) regButton.IsEnabled = true;
-                else regButton.IsEnabled = false;
-            }
-            else
-            {
-                regEmail.BackgroundColor = Color.FromHex("#fc8888");
-                formChecks[0] = false;
-                regButton.IsEnabled = false;
-            }
+            //if (Patterns.EmailAddress.Matcher(regEmail.Text).Matches())
+            //{
+            //    regEmail.BackgroundColor = Color.FromHex("#88fc8c");
+            //    formChecks[0] = true;
+            //    if (allChecksClear()) regButton.IsEnabled = true;
+            //    else regButton.IsEnabled = false;
+            //}
+            //else
+            //{
+            //    regEmail.BackgroundColor = Color.FromHex("#fc8888");
+            //    formChecks[0] = false;
+            //    regButton.IsEnabled = false;
+            //}
             
         }
 
