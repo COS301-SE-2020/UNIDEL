b833a299dad75b20dc6fc5e04c3bed6e83291449
Khakhu Ramakuela <56280844+u17283818@users.noreply.github.com>
2020-09-15 12:24:19 +0200

    Restore Register Page

--- mobile-application/UniDel/UniDel/UniDel/Views/RegisterPage.xaml.cs
+++ mobile-application/UniDel/UniDel/UniDel/Views/RegisterPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using Android.Util;
+using Android.Util;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using UniDel.Models;
@@ -236,7 +236,7 @@ namespace UniDel.Views
 
         public void OnRegEmailTextChanged(object sender, EventArgs args)
         {
-            /*if (Patterns.EmailAddress.Matcher(regEmail.Text).Matches())
+            if (Patterns.EmailAddress.Matcher(regEmail.Text).Matches())
             {
                 regEmail.BackgroundColor = Color.FromHex("#88fc8c");
                 formChecks[0] = true;
@@ -248,7 +248,7 @@ namespace UniDel.Views
                 regEmail.BackgroundColor = Color.FromHex("#fc8888");
                 formChecks[0] = false;
                 regButton.IsEnabled = false;
-            }*/
+            }
             
         }
 
