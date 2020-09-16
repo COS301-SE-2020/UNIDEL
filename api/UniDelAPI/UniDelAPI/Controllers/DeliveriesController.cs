e3504e5d9cde1529411405ad5331f3b6d972433e
William Tandwe <u15232672@tuks.co.za>
2020-09-16 03:04:26 +0200

    Added API key to API calls

--- api/UniDelAPI/UniDelAPI/Controllers/DeliveriesController.cs
+++ api/UniDelAPI/UniDelAPI/Controllers/DeliveriesController.cs
@@ -21,16 +21,26 @@ namespace UniDelAPI.Controllers
         }
 
         // GET: api/Deliveries
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Delivery>>> GetDeliveries()
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<Delivery>>> GetDeliveries([FromQuery] string k="")
         {
+            if (k != "UDL2Avv378jBBgd772hFSbbsfwUD")
+            {
+                return Unauthorized(new { message = "Request requires a valid API key"});
+            }
+
             return await _context.Deliveries.ToListAsync();
         }
 
         // GET: api/Deliveries/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Delivery>> GetDelivery(int id)
+        public async Task<ActionResult<Delivery>> GetDelivery(int id, [FromQuery] string k = "")
         {
+            if (k != "UDL2Avv378jBBgd772hFSbbsfwUD")
+            {
+                return Unauthorized(new { message = "Request requires a valid API key" });
+            }
+
             var delivery = await _context.Deliveries.FindAsync(id);
 
             if (delivery == null)
@@ -46,8 +56,13 @@ namespace UniDelAPI.Controllers
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [Route("~/api/Deliveries/PutDelivery/{id}")]
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutDelivery(int id, Delivery delivery)
+        public async Task<IActionResult> PutDelivery(int id, Delivery delivery, [FromQuery] string k = "")
         {
+            if (k != "UDL2Avv378jBBgd772hFSbbsfwUD")
+            {
+                return Unauthorized(new { message = "Request requires a valid API key" });
+            }
+
             if (id != delivery.DeliveryID)
             {
                 return BadRequest();
@@ -78,8 +93,13 @@ namespace UniDelAPI.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Delivery>> PostDelivery(Delivery delivery)
+        public async Task<ActionResult<Delivery>> PostDelivery(Delivery delivery, [FromQuery] string k = "")
         {
+            if (k != "UDL2Avv378jBBgd772hFSbbsfwUD")
+            {
+                return Unauthorized(new { message = "Request requires a valid API key" });
+            }
+
             _context.Deliveries.Add(delivery);
             await _context.SaveChangesAsync();
 
@@ -88,8 +108,13 @@ namespace UniDelAPI.Controllers
 
         // DELETE: api/Deliveries/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Delivery>> DeleteDelivery(int id)
+        public async Task<ActionResult<Delivery>> DeleteDelivery(int id, [FromQuery] string k = "")
         {
+            if (k != "UDL2Avv378jBBgd772hFSbbsfwUD")
+            {
+                return Unauthorized(new { message = "Request requires a valid API key" });
+            }
+
             var delivery = await _context.Deliveries.FindAsync(id);
             if (delivery == null)
             {
