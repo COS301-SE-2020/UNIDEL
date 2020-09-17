39d2e0e3a1e1d0211b52b67fb4c76c5a66a3a9e7
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2020-09-17 12:58:33 +0200

    Vehicle Mileage with push notifications enabled

--- mobile-application/UniDel/UniDel/UniDel/Views/VehicleMileage.xaml.cs
+++ mobile-application/UniDel/UniDel/UniDel/Views/VehicleMileage.xaml.cs
@@ -6,35 +6,19 @@ using System.Text;
 using Newtonsoft.Json;
 using UniDel.Data;
 using UniDel.Models;
+using UniDel.ViewModels;
 using Xamarin.Forms;
 
 namespace UniDel.Views
 {
     public partial class VehicleMileage : ContentPage
     {
-        //public string StrNumber
-        //{
-        //    get
-        //    {
-        //        if (Kilos == null)
-        //            return "";
-        //        else
-        //            return Kilos.ToString();
-        //    }
 
-        //    set
-        //    {
-        //        try
-        //        {
-        //            Kilos = int.Parse(value);
-        //        }
-        //        catch
-        //        {
-        //            Kilos = null;
-        //        }
-        //    }
-        //}
+        INotificationManager notificationManager;
 
+        Vehicle packet = null;   //new vehicle object to post
+
+        int id;
         public VehicleMileage()
         {
             InitializeComponent();
@@ -44,63 +28,118 @@ namespace UniDel.Views
         public async void btnLog_Clicked(System.Object sender, System.EventArgs e)
         {
             //make a get request to get all vehicles
-            //List<Vehicle> vehicles= null;
-            //Vehicle data = null;
-            //var httpClientHandler = new HttpClientHandler();
-            //httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
-            //var httpClient = new HttpClient(httpClientHandler);
-
-            //var response = await httpClient.GetStringAsync(Constants.BaseURL + "Vehicles/GetAll?" + Constants.Token);
-            //vehicles = JsonConvert.DeserializeObject<List<Vehicle>>(response);
-            //foreach (var item in vehicles)
-            //{
-            //    if (item.VehicleLicensePlate == logPlate.Text)
-            //    {
-            //        int id = item.VehicleID;
-            //        Vehicle vehicle_data = null;
-            //        var response2 = await httpClient.GetStringAsync(Constants.BaseURL + "Clients/" + id + "?" + Constants.Token);
-            //        vehicle_data = JsonConvert.DeserializeObject<Vehicle>(response2);
+            List<Vehicle> vehicles = null;
 
 
-            //        data.Add(new Vehicle()
-            //        {
-            //            VehicleID = vehicle_data.VehicleID,
-            //            VehicleMake = vehicle_data.VehicleMake,
-            //            VehicleLastService = vehicle_data.VehicleLastService,
-            //            VehicleLicensePlate = vehicle_data.VehicleLicensePlate,
-            //            VehicleLicenseDiskExpiry = vehicle_data.VehicleLicenseDiskExpiry,
-            //            VehicleModel = vehicle_data.VehicleModel,
-            //            VehicleNextMileageService = vehicle_data.VehicleNextMileageService,
-            //            VehicleVIN = vehicle_data.VehicleVIN,
-            //            VehicleMileage = int(logKM.Text),
-            //            VehicleNextDateService = vehicle_data.VehicleNextDateService
-            //        });
-            //    }
-            //}
-            ////post call
-            //Vehicle packet 
-            //httpClientHandler = new HttpClientHandler();
-            //httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            var httpClient = new HttpClient(httpClientHandler);
 
-            //httpClient = new HttpClient(httpClientHandler);
-            //string json = JsonConvert.SerializeObject(packet);
-            //StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            ////indicator.IsRunning = true;
-            ////indicator.IsVisible = true;
+            var response = await httpClient.GetStringAsync(Constants.BaseURL + "Vehicles/GetAll?" + Constants.Token);
+            vehicles = JsonConvert.DeserializeObject<List<Vehicle>>(response);
+            foreach (var item in vehicles)
+            {
+                if (item.VehicleLicensePlate == logPlate.Text)
+                {
+                    id = item.VehicleID;            //get the id of vehicle to update
+                                                    //make a get request for specific vehicle
+                    Vehicle vehicle_data = null;
 
-            ////API POST CALL TO CREATE
-            //var response2 = await httpClient.PostAsync("https://api.unideldeliveries.co.za/api/Deliveries/PutDelivery/" + QR_ID_Scanned + "?k=UDL2Avv378jBBgd772hFSbbsfwUD", content);
-            //if (response2.StatusCode == HttpStatusCode.NoContent)
-            //{
-            //    //await DisplayInfoChangedEventArgs("Confirmation in progress");
-            //    //await DisplayAlert("Confirming", "Confirmation in progress", "OK");
-            //    return;
-            //}
-            //else
-            //{
-            //    await DisplayAlert("Confirmation failed", "An error occured while confirming your package", "OK");
-            //}
+
+                    var response2 = await httpClient.GetStringAsync(Constants.BaseURL + "Vehicles/" + id + "?" + Constants.Token);
+                    vehicle_data = JsonConvert.DeserializeObject<Vehicle>(response2);
+                    Console.WriteLine(response2);
+
+
+                    packet = new Vehicle()
+                    {
+                        VehicleID = vehicle_data.VehicleID,
+                        VehicleMake = vehicle_data.VehicleMake,
+                        VehicleLastService = vehicle_data.VehicleLastService,
+                        VehicleLicensePlate = vehicle_data.VehicleLicensePlate,
+                        VehicleLicenseDiskExpiry = vehicle_data.VehicleLicenseDiskExpiry,
+                        VehicleModel = vehicle_data.VehicleModel,
+                        VehicleNextMileageService = vehicle_data.VehicleNextMileageService,
+                        VehicleVIN = vehicle_data.VehicleVIN,
+                        VehicleMileage = int.Parse(logKM.Text),
+                        VehicleNextDateService = vehicle_data.VehicleNextDateService
+                    };
+
+                }
+            }
+
+         //post call
+            httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+
+
+            httpClient = new HttpClient(httpClientHandler);
+            string json = JsonConvert.SerializeObject(packet);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //Console.WriteLine(json);
+            //API POST CALL TO CREATE
+            var response3 = await httpClient.PostAsync("https://api.unideldeliveries.co.za/api/Vehicles/Put/" + id + "?" + Constants.Token, content);
+            //Console.WriteLine(response3);
+            //Console.WriteLine("¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢");
+            if (response3.StatusCode == HttpStatusCode.NoContent)
+            {
+                await DisplayAlert("Update success", "Vehicle mileage successfully added, you may proceed to logout", "OK");             
+            }
+            else
+            {
+                await DisplayAlert("Update failed", "An error occured while updating the vehicle mileage", "OK");
+            }
+
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine(packet.VehicleMileage);
+            Console.WriteLine(packet.VehicleNextMileageService);
+            if (packet.VehicleMileage > packet.VehicleNextMileageService)
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("yes");
+                Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@");
+                notify();
+            }
+            else
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("no");
+                Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@");
+            }
 
         }
+
+
+        private void notify()
+        {
+
+                notificationManager = DependencyService.Get<INotificationManager>();
+                notificationManager.NotificationReceived += (sender, args) =>
+                {
+                    var evtData = (NotificationEventArgs)args;
+                    ShowNotification(evtData.Title, evtData.Message);
+                };
+
+
+                string title = "Vehicle Notification";
+                string message = packet.VehicleMake + " " + packet.VehicleModel + " is overdue a service";
+                notificationManager.ScheduleNotification(title, message);
+ 
+        }
+
+        private void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+                //stackLayout.Children.Add(msg);
+            });
+        }
     }
+
 }
